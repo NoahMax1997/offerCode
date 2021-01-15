cb7e47483446f16e5217900cf73c3c5f8fcbe62e
NoahMax1997 <1076444637@qq.com>
2021-01-15 23:30:41 +0800

    031

--- offer031StackPushAndPopVec.cpp
+++ offer031StackPushAndPopVec.cpp
@@ -1,24 +1,18 @@
 #include "headFile/frequent_head_file.h"
 class Solution {
 public:
-    stack<int> st;
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        int i=0,ii=0;
         bool ret=false;
-        int end=0;
-        while(!end){
-            while (st.empty()||ii<popped.size()&&st.top()!=popped[ii]&&i<pushed.size())
-            {
-                st.push(pushed[i++]);
-            }
-            if(ii<popped.size()&&!st.empty()&&popped[ii]==st.top()){
+        stack<int> st;
+        int i=0;
+        for(auto e:pushed){
+            st.push(e);
+            while(!st.empty()&&popped[i]==st.top()){
                 st.pop();
-                ii++;
-            }
-            if(ii==popped.size()||i==pushed.size()&&()){
-
+                i++;
             }
         }
+        return i==popped.size();
     }
 };
 int main()
