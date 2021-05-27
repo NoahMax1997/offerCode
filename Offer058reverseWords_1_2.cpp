44a13ee260225f608399f75a0a0591db7c5290bc
NoahMax1997 <1076444637@qq.com>
2021-05-28 00:39:27 +0800

    058

--- Offer058reverseWords_1_2.cpp
+++ Offer058reverseWords_1_2.cpp
@@ -1,25 +1,77 @@
 #include "headFile/frequent_head_file.h"
 class Solution {
 public:
-    size_t findChar(const string &s,int index,const char ch) noexcept{
-        for (size_t i = index; i < s.size(); i++)
+    string reverseWordsTwoPoints(string s) {
+        string ret;
+        int left = s.size() - 1,right = s.size() - 1;
+        while (left >= 0)
+        {
+            while (s[left] == ' ')
+            {
+                /* code */
+                left--;
+                right--;
+                if(left < 0){
+                    break;
+                }
+            }
+            if(left < 0){
+                break;
+            }
+            while (s[left] != ' ')
+            {
+                /* code */
+                left--;
+            }
+            ret = s.substr(left+1,right-left) +" ";
+            right = left;
+        }
+        int index = ret.size() - 1;
+        while (index >= 0 && ret[index] == ' ')
         {
             /* code */
-            if(ch == s[i]){
-                return i;
+            index--;
+        }
+        ret = ret.substr(0,index+1);
+        return ret;
+    }
+
+    string reverseWordsStack(string s){
+        stack<char> m_stack;
+        string ret = "";
+        for (int i = s.size() - 1; i >= 0; i--)
+        {
+            /* code */
+            if(s[i] == ' ' && !m_stack.empty()){
+                while (!m_stack.empty())
+                {
+                    /* code */
+                    ret += m_stack.top();
+                    m_stack.pop();
+                }
+                ret += " ";
+            }
+            else if(s[i] != ' '){
+                m_stack.push(s[i]);
             }
         }
-        return s.npos;
-    }
-    string reverseWords(string s) {
-        vector<string> vec;
-        int index = 0;
-        while (index < s.size())
+        while (!m_stack.empty())
         {
             /* code */
+            ret += m_stack.top();
+            m_stack.pop();
         }
-        
+        ret += " ";
+        int index = ret.size() - 1;
+        while (index >= 0 && ret[index] == ' ')
+        {
+            /* code */
+            index--;
+        }
+        ret = ret.substr(0,index+1);
+        return ret;
     }
+
 };
 int main()
 {
